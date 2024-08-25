2103051d7c88677b16cfe97f04acbecbcc4f2913
abdoelsayedDragon1 <abdo.com.a777@gmail.com>
2024-08-25 17:22:32 +0300

    first commit

---
+++ RefByValue/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RefByValue
+{
+    internal class Program
+    {
+        static void overRide(ref int[]arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = 10; 
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            int[] myarr= { 1, 2, 3, 5, 6, 7, 8, 9};
+            Console.WriteLine(myarr[5]);
+                overRide( ref myarr);
+            Console.WriteLine(myarr[0]);
+        }
+    }
+}
---
+++ StudyPass ByRef/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyPass_ByRef
+{
+    internal class Program
+    {
+        static void Swap(ref int A, ref int B)
+        {
+            A = 20;
+            B = 10;
+        }
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Swap(ref x, ref y);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            
+        }
+    }
+}
